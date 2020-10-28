d4a68f55aaff412ab3cba4f5980799dab80e7579
fsiniscalco10@stuy.edu <fsiniscalco10@stuy.edu>
2020-10-28 17:41:30 +0100

    updated

--- main.c
+++ main.c
@@ -8,11 +8,11 @@ int main(){
 	srand(time(NULL));
 
 	struct population *Data1;
-	Data1=new_population(abs(rand()),"New York");
+	Data1=new_population(abs(rand()),"USA");
 	struct population *Data2;
-	Data2=new_population(abs(rand()),"Rome");
+	Data2=new_population(abs(rand()),"China");
 	struct population *Data3;
-	Data3=new_population(abs(rand()),"London");
+	Data3=new_population(abs(rand()),"Europe");
 
 	print_population(Data1);
 	print_population(Data2);
